2d4e8d13511d1a6d5399d791e218e03ba3705ef2
Ibraheem Hameed <ibraheem.hameed@mail.utoronto.ca>
2023-08-03 18:38:35 -0400

    fixed

---
+++ hackstreet/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Searchbox from './components/searchbox';
+function App() {
+  return (
+    <div className="App">
+      <Searchbox/>
+    </div>
+  );
+}
+
+export default App;
