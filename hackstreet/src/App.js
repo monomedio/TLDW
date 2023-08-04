1489a2809298b1ddfbd8c15f2e7cb8ddd2f2c290
Ibraheem Hameed <ibraheem.hameed@mail.utoronto.ca>
2023-08-04 18:56:04 -0400

    more stuff done

--- hackstreet/src/App.js
+++ hackstreet/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBox from './components/SearchBox';
+import SummaryButton from './components/SummaryButton';
 function App() {
   return (
     <div className="App">
-    <div>
-      <SearchBox/>
-
+    <div className='row'>
+        <div className='left'>
+            <SearchBox/>
+        </div>
+        <div className='right'>
+            <SummaryButton/>
+        </div>
     </div>
     </div>
   );
--- hackstreet/src/components/SummaryButton/index.jsx
+++ hackstreet/src/components/SummaryButton/index.jsx
@@ -1,9 +1,72 @@
+     /*global chrome*/
 import React from "react";
+import 'react-awesome-button/dist/styles.css';
+import axios from "axios"
+import {
+    AwesomeButton,
+    AwesomeButtonProgress,
+    AwesomeButtonSocial,
+  } from 'react-awesome-button';
+
+
+function getCurrentTabUrl(callback) {
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
+    const url = tab.url;
+    callback(url);
+});
+}
+
+
+const handlePress = () => {
+    const fetchData = async () => {
+        try {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+            "id": "GxgqpCdOKak",
+            "token": "good girl",
+            "isExactMatch": true
+            });
+
+            var requestOptions = {
+            method: 'POST',
+            mode: "no-cors",
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+            };
+
+            fetch("https://6unmv3413l.execute-api.ca-central-1.amazonaws.com/Prod/hello", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+
+        } catch(error){
+            console.log(error)
+        }
+    }
+    
+    
+    const attemp2 = async () => {
+        const response = await axios.post("https://6unmv3413l.execute-api.ca-central-1.amazonaws.com/Prod/hello", 
+           {mode: "no-cors", body:
+           {
+            "id": "GxgqpCdOKak",
+            "token": "good girl",
+            "isExactMatch": true
+            }})
+            console.log(response.data)
+        }
+    attemp2()
+
+}
+
 
 const SummaryButton = () => {
     return (
-        <></>
-    
+        <><AwesomeButton type="primary" onPress={handlePress} >Summarize</AwesomeButton></>
     )
 }
 
