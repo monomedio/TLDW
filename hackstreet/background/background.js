98310f4516d9a1b0b12ac96487317a52cfbfb6b8
Ibraheem014 <66434732+Ibraheem014@users.noreply.github.com>
2023-08-09 00:17:59 -0400

    changed manifest

--- hackstreet/background/background.js
+++
@@ -1,8 +0,0 @@
-/*global chrome*/
-
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  console.log(typeof(tab));
-}
